b1d0d85280a0d7e1d2633ae64ee849f75e813792
Arun9123n <145862247+Arun9123n@users.noreply.github.com>
2024-01-24 15:26:50 +0530

    Add files via upload

---
+++ fspromises.js
@@ -0,0 +1,11 @@
+// const fs = require("fs/promises")
+
+// async function readFiles(){
+//     try{
+//         const data = await fs.readFile("hello.text","utf-8")
+//         console.log(data)
+//     } catch(e){
+//         console.log(e)
+//     }
+// }
+// readFiles();
